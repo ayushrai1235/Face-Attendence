c3e23f9f808094efecbdc59dcfd7f9b285572654
ayushrai1235 <prathibharai64@gmail.com>
2024-09-14 14:43:43 +0530

    face attendance system

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pickle
-
 import cv2
 import os
 import cvzone
 import numpy as np
 import face_recognition
+
 
 cap = cv2.VideoCapture(0)
 cap.set(3,640)
@@ -27,34 +27,36 @@ encodeListKnown, studentName = encodeListKnownwithname
 
 while True:
     success, img = cap.read()
-    imgbg[162:162+480,55:55+640] = img
-    imgbg[44:44+633,808:808+414] = imgModelist[3]
+
+
 
     imgs = cv2.resize(img,(0,0),None,0.25, 0.25)
     imgs = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     facecur = face_recognition.face_locations(imgs)
-    encodecur = face_recognition.face_encodings(imgs,facecur)
+    encodecur = face_recognition.face_encodings(imgs , facecur)
 
 
-    for encface, faceloc in zip(encodecur,facecur):
+    for encface, (y1,x2,y2,x1) in zip(encodecur,facecur):
         matches = face_recognition.compare_faces(encodeListKnown,encface)
         facename = face_recognition.face_distance(encodeListKnown, encface)
         # print("Matches",matches)
         # print(facename)
+        imgbg[162:162+480,55:55+640] = img
+
+        imgbg[44:44+633,808:808+414] = imgModelist[1]
 
         matchindex = np.argmin(facename)
         # print("match name :",matchindex)
+        # print(f"Face Location:{(x,y,w,h)}")
 
         if matches[matchindex]:
-            # print("FACE DETECTED")
-            # print(studentName[matchindex])
+            print("FACE DETECTED")
+            print(studentName[matchindex])
 
-            y1, x2, y2, x1 = faceloc
-            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
             w = x2-x1
             h = y2-y1
-            x1=55 + x1
+            x1= 55 + x1
             y1 = 162 + y1
             bbox = (x1, y1, w , h)
             imgbg = cvzone.cornerRect(imgbg, bbox, rt=0)
@@ -63,7 +65,6 @@ while True:
 
 
 
-
-
     cv2.imshow("face attendance", imgbg)
+
     cv2.waitKey(1)
